ad80f4051e93131a629cf2dc694144a725f328ca
warara <szwyn3211@gmail.com>
2021-01-27 10:22:23 +0900

    layout.html added

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = "top"
     # htmlを表示
-    return render_template('index.html')
+    return render_template('index.html', title=title)
 
 if __name__ == '__main__':
     app.run()
